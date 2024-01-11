387cea0ca60e77b34fb4ca41fb8795f0702208af
Geisher Bernabe <gei@insites.io>
2024-01-12 02:14:22 +0800

    Dashboard page deployment

--- js/about.bb7a902a.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkmy_fake_store"]=self["webpackChunkmy_fake_store"]||[]).push([[443],{833:function(e,n,t){t.r(n),t.d(n,{default:function(){return k}});var a=t(252);const r={class:"about"},s=(0,a._)("h1",null,"This is an about page",-1),u=[s];function c(e,n){return(0,a.wg)(),(0,a.iD)("div",r,u)}var o=t(744);const f={},i=(0,o.Z)(f,[["render",c]]);var k=i}}]);
-//# sourceMappingURL=about.bb7a902a.js.map
